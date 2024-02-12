6d91cad3c4370e8b1c3fbb7b7e5d718093fbea47
matokvekal <mictavim@gmail.com>
2024-02-12 15:07:28 +0200

    Update Weather.jsג

--- react-exercise-1/src/Weather/Weather.js
+++ react-exercise-1/src/Weather/Weather.js
@@ -6,7 +6,7 @@ function Weather() {
 		<div className="Weather">
 			<p>
 				Use the following API to display the hottest and coldest days:<br />
-				<code>https://netcraft2.s3-eu-west-1.amazonaws.com/weather.json</code>
+				<code>/weather.json</code>
 			</p>
 			<div className="Weather__stats">
 				<strong>Hottest day is:</strong>
